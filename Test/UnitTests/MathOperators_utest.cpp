070ecdbae6389b699e966795b1b5d53150fecdf6
Silmaen <genteur.slayer@laposte.net>
2019-12-04 22:26:23 +0100

    Correct Build for Clang

--- Test/UnitTests/MathOperators_utest.cpp
+++ Test/UnitTests/MathOperators_utest.cpp
@@ -6,31 +6,31 @@ using namespace optim;
 
 TEST(MathOperators, sqrtnoexcept)
 {
-    EXPECT_EQ(optim::sqrt(static_cast<u8>(4)), 2u);
-    EXPECT_EQ(optim::sqrt(static_cast<u16>(4)), 2u);
-    EXPECT_EQ(optim::sqrt(static_cast<u32>(4)), 2u);
-    EXPECT_EQ(optim::sqrt(static_cast<u64>(4)), 2u);
-    EXPECT_EQ(optim::sqrt(static_cast<s8>(4)), 2);
-    EXPECT_EQ(optim::sqrt(static_cast<s16>(4)), 2);
-    EXPECT_EQ(optim::sqrt(static_cast<s32>(4)), 2);
-    EXPECT_EQ(optim::sqrt(static_cast<s64>(4)), 2);
-    EXPECT_EQ(optim::sqrt(4.0f), 2.0f);
-    EXPECT_EQ(optim::sqrt(4.0), 2.0);
+    EXPECT_EQ(sqrt(static_cast<u8>(4)), 2u);
+    EXPECT_EQ(sqrt(static_cast<u16>(4)), 2u);
+    EXPECT_EQ(sqrt(static_cast<u32>(4)), 2u);
+    EXPECT_EQ(sqrt(static_cast<u64>(4)), 2u);
+    EXPECT_EQ(sqrt(static_cast<s8>(4)), 2);
+    EXPECT_EQ(sqrt(static_cast<s16>(4)), 2);
+    EXPECT_EQ(sqrt(static_cast<s32>(4)), 2);
+    EXPECT_EQ(sqrt(static_cast<s64>(4)), 2);
+    EXPECT_EQ(sqrt(4.0f), 2.0f);
+    EXPECT_EQ(sqrt(4.0), 2.0);
 }
 
 TEST(MathOperators, sqrtexcept)
 {
-    bool a;
-    EXPECT_THROW({ a = optim::sqrt(static_cast<s8>(-4)) == 2; }, optim::Exception);
-    EXPECT_THROW({ a = optim::sqrt(static_cast<s16>(-4)) == 2; }, optim::Exception);
-    EXPECT_THROW({ a = optim::sqrt(static_cast<s32>(-4)) == 2; }, optim::Exception);
-    EXPECT_THROW({ a = optim::sqrt(static_cast<s64>(-4)) == 2; }, optim::Exception);
-    EXPECT_THROW({ a = optim::sqrt(-4.0f) == 2; }, optim::Exception);
-    EXPECT_THROW({ a = optim::sqrt(-4.0) == 2; }, optim::Exception);
+    bool a=false;
+    EXPECT_THROW({ a = sqrt(static_cast<s8>(-4)) == 2; }, Exception);
+    EXPECT_THROW({ a = sqrt(static_cast<s16>(-4)) == 2; }, Exception);
+    EXPECT_THROW({ a = sqrt(static_cast<s32>(-4)) == 2; }, Exception);
+    EXPECT_THROW({ a = sqrt(static_cast<s64>(-4)) == 2; }, Exception);
+    EXPECT_THROW({ a = isfEqual(sqrt(-4.0f), 2); }, Exception);
+    EXPECT_THROW({ a = isfEqual(sqrt(-4.0), 2); }, Exception);
     if (a) return;
 }
 
 TEST(MathOperators, log2i)
 {
-    EXPECT_EQ(optim::log2i(static_cast<u32>(4)), 3u);
+    EXPECT_EQ(log2i(static_cast<u32>(4)), 3u);
 }
