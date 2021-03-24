d638f43b773f0828634c138b9d297e2e0a1a0b98
Silmaen <genteur.slayer@laposte.net>
2021-03-24 17:40:35 +0100

    building script simplification

--- cmake/OptimCustomTargets.cmake
+++ cmake/OptimCustomTargets.cmake
@@ -3,12 +3,14 @@ if (CMAKE_BUILD_TYPE MATCHES "Debug")
     set(CONFIG_FLAG "-g")
 endif()
 
+find_program(ZIP_BIN zip PATHS ${OPP_COMPILER_PATH} ${OPP_ADDITIONAL_PATH})
+
 # packaging:
 add_custom_target(
         Packaging
         COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_BINARY_DIR}/documentation
         COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/doc/build/html ${CMAKE_BINARY_DIR}/documentation
-        COMMAND cd ${CMAKE_BINARY_DIR} &&  zip -r optimpp.zip bin lib documentation
+        COMMAND cd ${CMAKE_BINARY_DIR} &&  ${ZIP_BIN} -r optimpp.zip bin lib documentation
         COMMENT "Package Application"
         VERBATIM
         DEPENDS optimpp Documentation
